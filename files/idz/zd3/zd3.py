28d37eec7848446a6cee497f583546e38b245f62
Timofej8971 <78484537+Timofej8971@users.noreply.github.com>
2021-12-21 12:31:32 +0300

    скобочки поправил

--- files/idz/zd3/zd3.py
+++ files/idz/zd3/zd3.py
@@ -19,13 +19,15 @@ if __name__ == '__main__':
             w = os.access(file_path, os.W_OK)
             x = os.access(file_path, os.X_OK)
             print('Выбранный файл имеет следующие параметры доступа:  \n',
-                  f, '\n', r, '\n', w, '\n', x, '\n')
+                  f, '\n', r, '\n', w, '\n', x, '\n'
+                )
 
         elif command == 'help':
             print('Cписок команд:\n'
                   'exit - выход из программы'
                   'check - проверка доступа'
-                  'help - помощь')
+                  'help - помощь'
+                 )
 
         else:
             print(f"Неизвестная команда {command}", file=sys.stderr)
